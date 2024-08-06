fc38a1bb3e8955e51c9f202ebcbaa712dae7ac54
siouxweb-art <ncaceresb004@gmail.com>
2024-08-06 15:48:16 +0200

    Update ejercicios11.js

--- ejercicios1/ejercicios11.js
+++ ejercicios1/ejercicios11.js
@@ -1,16 +1,29 @@
-const mixedElements = [6, 1, 'Marvel', 1, 'hambuerguesa', '10', 'Prometeo', 8, 'Hola mundo'];
-
-function averageWord(list){
+function sumArray(arr) {
     let sum = 0;
-    for (let i=0; i <mixedElements.length; i++){
-        if ( mixedElements[i].length >1 ) {
-            let element = mixedElements[i].length;
-        }
-       sum += mixedElements[i] + element;
+    for (let i = 0; i < arr.length; i++) {
+        if(typeof arr[i] === 'string'){
+        sum += arr[i];
     }
+   
     return sum;
 }
+}
 
- 
+const mixedElements = [6, 1, 'Marvel', 1, 'hambuerguesa', '10', 'Prometeo', 8, 'Hola mundo'];
 
-console.log(element)
+/*function averageWord(list){
+    if (typeof List === 'string'){
+        for (let i=0; i<mixedElements.length; i++){
+            let element = mixedElements[i].length;
+            
+        }
+        let sum =0;
+        sum += mixedElements[i]+element;
+        
+    }
+    
+}
+let newMixed = averageWord(mixedElements);
+console.log(newMixed)*/
+
+console.log(sumArray(mixedElements))
